29252352c0fedfe616991e033b2f4dbe3c9b7a9c
Ruslan Makarov <ruslan.makarov@gmail.com>
2023-10-31 13:55:09 +0700

    feat: github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 // import { URL, fileURLToPath } from "node:url";
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/base-website/",
   build: {
     target: "esnext",
   },
