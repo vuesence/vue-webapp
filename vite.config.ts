4480fa6068283b2d5dd7614ad781c7fe18b2427e
Ruslan Makarov <ruslan.makarov@gmail.com>
2023-11-21 13:53:04 +0700

    fix: add base `vue-webapp`

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 // import { URL, fileURLToPath } from "node:url";
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/vue-webapp/",
   build: {
     target: "esnext",
   },
