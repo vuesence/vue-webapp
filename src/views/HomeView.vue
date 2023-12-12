03786c4b966dae0550ad7f3089d86d2199ce003c
Ruslan Makarov <ruslan.makarov@gmail.com>
2023-12-12 20:57:48 +0300

    chore: styling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,12 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
-import { api } from "@/services/api";
 
 interface IOption {
   name: string
   value: string
 }
 
-const apiData = ref();
-const jsonRpcData = ref();
-
-onMounted(async () => {
-  jsonRpcData.value = await api.utils.testJsonRpc();
-  apiData.value = await api.utils.testRest();
-});
+// api-placeholder
 
 const options: IOption[] = [];
 </script>
@@ -21,7 +14,6 @@ const options: IOption[] = [];
 <template>
   <div>
     <h2>Congratulations with scaffolding your vue webapp!</h2>
-    <hr>
     <h3 v-if="options.length">
       Selected options:
     </h3>
@@ -34,18 +26,12 @@ const options: IOption[] = [];
         <span class="value">{{ option.value }}</span>
       </li>
     </ul>
-    <hr />
-    <h3>API data:</h3> <p>{{ apiData }}</p>
-    <hr />
-    <h3>JSON-RPC data:</h3>
-    <p class="json-rpc-data">
-      {{ jsonRpcData }}
-    </p>
   </div>
 </template>
 
 <style lang="scss" scoped>
 ul {
+  color: var(--vp-c-text-2);
   border: 1px solid var(--vp-c-divider);
   margin-bottom: 2em;
   li {
@@ -58,9 +44,5 @@ ul {
       width: 50%;
     }
   }
-}
-.json-rpc-data {
-  max-width: 80vw;
-  overflow-x: auto;
 }
 </style>
