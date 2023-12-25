221cbbd73b7864c87a6743853b5d2a6218e68cc0
Ruslan Makarov <ruslan.makarov@gmail.com>
2023-12-25 14:06:47 +0300

    chore: clean up

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,14 +2,11 @@ import antfu from "@antfu/eslint-config";
 
 export default antfu({
   ignores: [".vscode/settings.json", ".vscode/settings.json/**", "src/assets/locales/*.json", "src/assets/locales/*.json/**"],
-  // stylistic: false,
   rules: {
     "ts/semi": "off",
     "curly": ["error", "all"],
     "no-console": "off",
     "no-alert": "off",
-    // "ts/brace-style": ["error", "1tbs", { allowSingleLine: true }],
-    // "ts/brace-style": "off",
     "vue/html-self-closing": "off",
     "style/semi": ["error", "always"],
     "style/indent": 2, // 4, or 'tab'
@@ -18,11 +15,5 @@ export default antfu({
       "double",
     ],
     "style/brace-style": ["error", "1tbs", { allowSingleLine: true }],
-    // "style/brace-style": "off",
-  },
-  stylistic: {
-  // "style/quotes": "double", // or 'single'
-    // "style/brace-style": "off",
-    // "object-curly-newline": "off",
   },
 });
